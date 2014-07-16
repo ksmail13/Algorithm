5b609a41881041ecc73197dee1ec8e3e245e07fa
ksmail13_linux <ksmail13@gmail.com>
2014-07-16 23:10:08 +0900

    quicksort added

--- sort_compare.cpp
+++ sort_compare.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
 
 #define LIMIT 10
 
@@ -33,32 +36,35 @@ void swap(int *a, int *b)
 
 void quicksort(int *arr, int left, int right) {
     if(!arr || (left >= right)) return ;
-    int start = left, end = right;
+    int start = left, end = right++;
     int pivot = arr[start];
     cout << "start left->" << left << " right->" << right << endl;
-    while(left < right) {
+    do {
         while(left <= end && arr[++left] < pivot) {}
-        while(right >= start && arr[right--] > pivot) {}
+        while(right > start && arr[--right] > pivot) {}
         
-        cout << "left : " << left << " right:" << right+1 << endl;
-        if(left < right) {
-            swap(&arr[left], &arr[right+1]);
-        }
-        //swap(&arr[left], &arr[right+1]);
-        
-    } 
+        cout << "left : " << left << " right:" << right << endl;
+		if(left < right)
+        	swap(&arr[left], &arr[right]);
+    } while(left < right) ;
     
-    cout << "start : " << start << " right:" << right+1 << endl;
-    swap(&arr[start], &arr[right+1]);
+    cout << "start : " << start << " right:" << right << endl;
+    swap(&arr[start], &arr[right]);
     cout << "left sort" << endl;
-    quicksort(arr, start, right-1);
+	if(right != end)
+    	quicksort(arr, start, right);
     cout << "right sort" << endl;
+	if(right+1 != start)
     quicksort(arr, right+1,end);
 }
 
 void printarr(int *arr, int len) {
     if(!arr) return;
-    for(int i=0;i<LIMIT;i++) {
+	for(int i=0;i<len;i++) {
+        printf("%d ",i);
+    }
+	putchar('\n');
+    for(int i=0;i<len;i++) {
         printf("%d ", arr[i]);
     }
     putchar('\n');
