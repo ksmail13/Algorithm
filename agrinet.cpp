a590ddf5a5a7a601ed2f304ccdb1356af6c522e9
ksmail13_mac <ksmail13@gmail.com>
2014-07-17 21:43:33 +0900

    kruskal
    find cycle

--- agrinet.cpp
+++ agrinet.cpp
@@ -14,7 +14,7 @@ typedef struct edge {
 }Edge;
 
 int edgecmp(const void *data1, const void *data2);
-
+int dfs(int map[LIMIT][LIMIT], int size, int start_point int search);
 
 int main(int argc, char *argv[]) {
     int map[LIMIT][LIMIT] = {};
@@ -34,14 +34,51 @@ int main(int argc, char *argv[]) {
 			edge_arr[edge_idx].len = map[i][j];
 			edge_arr[edge_idx].p1 = i;
 			edge_arr[edge_idx++].p2 = j;
+            map[i][j] = 0;
 		}
 	}
 
 	qsort((void *)edge_arr, edge_idx, sizeof(Edge), edgecmp);
 
+    for(int i=0;i<edge_idx;i++) {
+        Edge temp = edge_arr[edge_idx];
+        int srch_pnt = temp.p1;
+        int pass = 0;
+        
+        for(int j=0;j<house_cnt;j++) {
+            int k = j;
+            while(map[srch_pnt][k]) {
+                if(j == temp.p2) {
+                    pass = 1;
+                    break;
+                }
+                else {
+                    srch_pnt = k;
+                    
+                }
+            }
+        }
+    }
+
 	return 0;
 }
 
+int dfs(int map[LIMIT][LIMIT], int size, int start_point int search) {
+    if(map) {
+        int i, result = 0;
+        for(i=0;i<size;i++) {
+            if(map[start_point][i] && i==search ) {
+                if(result = dfs(map, size, i, search)) {
+                    return result;
+                }
+            }
+        }
+        
+        return 0;
+    }
+    
+    return 0;
+} 
 
 
 int edgecmp(const void *data1, const void *data2) {
